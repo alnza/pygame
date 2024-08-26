26fd0a008149fdf0e8e20dfd449dbd6055ae7c09
wayne <eliutov22@gmail.com>
2024-08-26 16:40:45 +0500

    ADD ping pong

--- example1.py
+++ example1.py
@@ -1,26 +1,31 @@
-import pygame
+import pygame as pg
+import sys
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+pg.init()
+sc = pg.display.set_mode((400, 300))
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+pg.mixer.music.load('1.ogg')
+# pg.mixer.music.play()
+sound1 = pg.mixer.Sound('2.ogg')
+sound2 = pg.mixer.Sound('3.ogg')
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+while 1:
+    for i in pg.event.get():
+        if i.type == pg.QUIT:
+            sys.exit()
+        elif i.type == pg.MOUSEBUTTONUP:
+            if i.button == 1:
+                sound1.play()
+            elif i.button == 3:
+                sound2.play()
+        elif i.type == pg.KEYUP:
+            if i.key == pg.K_1:
+                pg.mixer.music.pause()
+            elif i.key == pg.K_2:
+                pg.mixer.music.unpause()
+                pg.mixer.music.set_volume(0.5)
+            elif i.key == pg.K_3:
+                pg.mixer.music.unpause()
+                pg.mixer.music.set_volume(1)
 
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
+    pg.time.delay(20)
